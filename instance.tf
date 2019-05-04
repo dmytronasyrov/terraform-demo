606d0b41df660019ebd85d390d1062f8bfa85287
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-05-04 22:09:58 +0300

    add output ip

--- instance.tf
+++ instance.tf
@@ -8,6 +8,10 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.demo_access_key.key_name}"
 
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.private_ip} >> private_ips"
+  }
+
   provisioner "file" {
     source = "script.sh"
     destination = "/tmp/script.sh"
@@ -25,4 +29,8 @@ resource "aws_instance" "example" {
     private_key = "${file("${var.PATH_TO_PRIV_KEY}")}"
     timeout = "10m"
   }
+}
+
+output "ip" {
+  value = "${aws_instance.example.public_ip}"
 }
