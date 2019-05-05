e58955cadbb620f43840af080c85f3c235a3a728
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-05-05 23:20:09 +0300

    add vpc and nat

---
+++ SG.ssh.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "allow-ssh" {
+  name = "allow-ssh"
+  description = "SG that allows SSH and all egress traffic"
+  vpc_id = "${aws_vpc.main.id}"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["94.125.120.28/32"]
+  }
+
+  tags {
+    Name = "allow-ssh"
+  }
+}
