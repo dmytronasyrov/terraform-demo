4c8db2b21e37dbc88ddaa523385277d7abe2928d
Dmytro Nasyrov <dmytro@pharosproduction.com>
2019-05-12 23:18:30 +0300

    add rds; add route53

--- route53.tf
+++ route53.tf
@@ -4,32 +4,33 @@ resource "aws_route53_zone" "pharos-production" {
 
 resource "aws_route53_record" "server1-record" {
   zone_id = "${aws_route53_zone.pharos-production.zone_id}"
-  name = "server1.pharos.production"
-  type = "A"
-  ttl = "300"
+  name    = "server1.pharos.production"
+  type    = "A"
+  ttl     = "300"
   records = ["8.8.8.8"]
 }
 
 resource "aws_route53_record" "www-record" {
   zone_id = "${aws_route53_zone.pharos-production.zone_id}"
-  name = "www.pharos.production"
-  type = "A"
-  ttl = "300"
+  name    = "www.pharos.production"
+  type    = "A"
+  ttl     = "300"
   records = ["8.8.8.8"]
 }
 
 resource "aws_route53_record" "mail1-record" {
-   zone_id = "${aws_route53_zone.pharos-production.zone_id}"
-   name = "pharos.production"
-   type = "MX"
-   ttl = "300"
-   records = [
-     "1 aspmx.l.google.com.",
-     "5 alt1.aspmx.l.google.com.",
-     "5 alt2.aspmx.l.google.com.",
-     "10 aspmx2.googlemail.com.",
-     "10 aspmx3.googlemail.com."
-   ]
+  zone_id = "${aws_route53_zone.pharos-production.zone_id}"
+  name    = "pharos.production"
+  type    = "MX"
+  ttl     = "300"
+
+  records = [
+    "1 aspmx.l.google.com.",
+    "5 alt1.aspmx.l.google.com.",
+    "5 alt2.aspmx.l.google.com.",
+    "10 aspmx2.googlemail.com.",
+    "10 aspmx3.googlemail.com.",
+  ]
 }
 
 output "ns-servers" {
